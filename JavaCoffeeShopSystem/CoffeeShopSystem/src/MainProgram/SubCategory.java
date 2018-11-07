95a37e20c4d4aa03e593f8eb7cad14e6595f8270
Andre Darsono <43400246+HikariRazer@users.noreply.github.com>
2018-11-07 14:23:21 +0700

    Add files via upload
    With Databases Package

---
+++ JavaCoffeeShopSystem/CoffeeShopSystem/src/MainProgram/SubCategory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainProgram;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Andre Darsono
+ */
+public class SubCategory extends JFrame {
+
+    public SubCategory() {
+        initComponent();
+    }
+
+    public void initComponent() {
+        setSize(1400, 900);
+    }
+}
